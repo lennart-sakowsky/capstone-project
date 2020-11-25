5ec494f89f9f04563147682e7eaaafb920645daf
lennart-sakowsky <lsakowsky@gmx.de>
2020-11-25 16:09:24 +0100

    Add POST for user and first draft of GET

--- mapit-backend/src/Controller/UserController.php
+++ mapit-backend/src/Controller/UserController.php
@@ -2,20 +2,37 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/user", methods={"GET"})
      */
-    public function index(): Response
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+    public function index(): JsonResponse {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        $response = [];
+
+        foreach($users as $user) {
+            $response[] = $user->toArray();
+        }
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * @Route("/user", methods={"POST"})
+     */
+    public function create(Request $request, UserRepository $repository) {
+        $postData = json_decode($request->getContent(), true);
+
+        $user = $repository->createUser($postData['name'], $postData['email'], $postData['password']);
+
+        return new JsonResponse($user->toArray());
     }
 }
