a40c57e7c5ba9a9ecd125903bfc1770b54017ba7
lennart-sakowsky <lsakowsky@gmx.de>
2020-11-25 21:40:24 +0100

    Try variation of JsonResponse, error persists: toArray() undefined - revert to initial attempt

--- mapit-backend/src/Controller/UserController.php
+++ mapit-backend/src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
