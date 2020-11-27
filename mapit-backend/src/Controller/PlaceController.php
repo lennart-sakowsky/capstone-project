7fbd386fc55f3ffcd5f3731c71ffff2ea25637d6
lennart-sakowsky <lsakowsky@gmx.de>
2020-11-27 16:37:43 +0100

    Add Place Entity and Controller

---
+++ mapit-backend/src/Controller/PlaceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlaceController extends AbstractController
+{
+    /**
+     * @Route("/place", name="place")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PlaceController.php',
+        ]);
+    }
+}
---
+++ mapit-backend/src/Entity/Place.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlaceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlaceRepository::class)
+ */
+class Place
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $zipcode;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $lat;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $lng;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getZipcode(): ?string
+    {
+        return $this->zipcode;
+    }
+
+    public function setZipcode(string $zipcode): self
+    {
+        $this->zipcode = $zipcode;
+
+        return $this;
+    }
+
+    public function getLat(): ?int
+    {
+        return $this->lat;
+    }
+
+    public function setLat(int $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getLng(): ?int
+    {
+        return $this->lng;
+    }
+
+    public function setLng(int $lng): self
+    {
+        $this->lng = $lng;
+
+        return $this;
+    }
+}
