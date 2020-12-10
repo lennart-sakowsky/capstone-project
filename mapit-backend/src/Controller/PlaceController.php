c301583db310fa196241432dcf68bf5c8b576faf
lennart-sakowsky <lsakowsky@gmx.de>
2020-12-10 13:07:42 +0100

    Delete unnecessary endpoints

--- mapit-backend/src/Controller/PlaceController.php
+++ mapit-backend/src/Controller/PlaceController.php
@@ -8,42 +8,11 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Serializer\PlaceSerializer;
 use App\Serializer\TagSerializer;
-use App\Repository\PlaceRepository;
 use App\Services\FindPlace;
 use App\Services\FindSortTags;
 
 class PlaceController extends AbstractController
 {
-    /**
-     * @Route("/place", methods={"GET"})
-     */
-    public function index(PlaceRepository $placeRepository, PlaceSerializer $placeSerializer): JsonResponse
-    {
-        $places = $placeRepository->findAll();
-
-        return new JsonResponse(
-            $placeSerializer->serialize($places),
-            JsonResponse::HTTP_OK,
-            [],
-            true
-        );
-    }
-
-    /**
-     * @Route("/place"), methods={"POST"}
-     */
-    public function create(Request $request, PlaceRepository $placeRepository, PlaceSerializer $placeSerializer): JsonResponse {
-        $place = $placeSerializer->deserialize($request->getContent());
-        $placeRepository->save($place);
-
-        return new JsonResponse(
-            $placeSerializer->serialize($place),
-            JsonResponse::HTTP_OK,
-            [],
-            true
-        );
-    }
-
     /**
      * @Route("/place/find"), methods={"POST"}
      */
