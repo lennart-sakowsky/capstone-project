ee2f3764b94040423356887002efc9635e569eb9
lennart-sakowsky <lsakowsky@gmx.de>
2020-12-11 14:42:34 +0100

    Small refactoring of PlaceSearch, delete useState

--- mapit-frontend/src/components/PlaceSearch.js
+++ mapit-frontend/src/components/PlaceSearch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import L from "leaflet";
 import * as ELG from "esri-leaflet-geocoder";
 import { useMap } from "react-leaflet";
