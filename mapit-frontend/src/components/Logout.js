ec3a08d7738bae5dd610bde6a2bda85cc59790fe
lennart-sakowsky <lsakowsky@gmx.de>
2020-12-17 23:48:05 +0100

    Small refactor of Navigation.js, delete old code

--- mapit-frontend/src/components/Logout.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/macro";
-
-export default function Logout() {
-  return <LogoutSymbol>L</LogoutSymbol>;
-}
-
-const LogoutSymbol = styled.div`
-  color: white;
-`;
--- mapit-frontend/src/components/navigation/Navigation.js
+++ mapit-frontend/src/components/navigation/Navigation.js
@@ -1,29 +1,19 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import SearchTagInput from "./../SearchTagInput";
-import Logout from "./../Logout";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { BiListUl } from "react-icons/bi";
 
 export default function Navigation({ updateTaggedPlaces }) {
   return (
     <NavBar>
-      {/* <NavList>
-        <NavListItem> */}
       <Link to={"/places"}>
         <BiListUl onClick={() => updateTaggedPlaces([])} />
       </Link>
-      {/* </NavListItem>
-        <NavListItem> */}
       <SearchTagInput updateTaggedPlaces={updateTaggedPlaces} />
-      {/* </NavListItem>
-        <NavListItem> */}
       <Link to={"/"}>
-        {/* <Logout /> */}
         <RiLogoutBoxRLine />
       </Link>
-      {/* </NavListItem>
-      </NavList> */}
     </NavBar>
   );
 }
