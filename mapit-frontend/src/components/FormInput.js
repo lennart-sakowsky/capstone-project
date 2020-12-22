762a894e05a8bee4fe6b9d1a97a150bf8d00bb83
lennart-sakowsky <lsakowsky@gmx.de>
2020-12-22 19:53:54 +0100

    Add reusable component: FormInput

---
+++ mapit-frontend/src/components/FormInput.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+FormInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  placeholder: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["text", "password"]),
+  value: PropTypes.any,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default function FormInput({
+  name,
+  type = "text",
+  placeholder,
+  onChange,
+  value,
+  children,
+  label,
+}) {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+      />
+    </>
+  );
+}
